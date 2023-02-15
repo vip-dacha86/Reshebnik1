44d2bcd95f0656e32151c9c6b175c7ef82b78809
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-15 14:59:30 +0500

    task_8

---
+++ task8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу которая принимает на вход два числа ипроверяетявляется ли одно число квадратом другого
+// 5,25->Yes 4,16->yes  25,5->yes 8,9->no  
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+if (a*a==b || Math.Pow(b,2) ==a)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
