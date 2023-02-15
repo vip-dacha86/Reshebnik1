1d24af09cedbd8c3f3d28d88e143b04de1971e3a
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-15 11:16:27 +0500

    task_2/

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Два целых числа выбрать наибольшее и наименьшее число из них.
+Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine("max = "+numberA + " min = " +numberB);
+} 
+else
+{
+    Console.WriteLine("max = "+numberB + " min = " +numberA);
+}
