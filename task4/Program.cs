9843ab4ef5f1faf688c6c90f12a99d4b039e2028
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-15 12:03:27 +0500

    task_4

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу которая выводит случайное число.
+// из отрезка [10,99] и показывает наибольшую цифру числа.
+Console.Clear();
+int number=new Random().Next(10,100);//number=78 
+int A1 = number/10;//a1=78/10=7
+int A2 =number%10;//a2=78%10=8
+int max=A1;
+if (max < A2)
+max=A2;
+Console.WriteLine($"максимальная цифра {number } -> {max}");
