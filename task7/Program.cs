5ff22b1134798c719e1cf278e6b67647b837f732
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-15 14:18:06 +0500

    task_7

---
+++ task7/Program.cs
@@ -0,0 +1,9 @@
+﻿// напишите прорамму которая принимает на вход число и проверяет 
+//кратно ли оно одновременно 7 и 23.
+//if((a>b||a<c)&&(x>b||x<c)||d==5)
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if(num%7 == 0 && num%23 == 0)
+Console.WriteLine($"{num}->Yes");
+else
+Console.WriteLine($"{num}->No");
