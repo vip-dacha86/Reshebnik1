ef5edc53c05e498d7b0a02a1b8b423f5b8c70d17
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-15 11:38:30 +0500

    task_3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать все целые числа от -N до N
+Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine());
+int y = x;
+x = -x;
+while(x <=y)
+{
+    Console.Write(x+",");
+x++; // X=X+1;
+}
+
