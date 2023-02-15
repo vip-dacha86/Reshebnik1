57d28d88524c52e418f57e57559a70d04dd08e8c
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-15 10:25:25 +0500

    task_1/

---
+++ task1/Program.cs
@@ -0,0 +1,5 @@
+﻿//Вывести квадрат целого числа
+Console.WriteLine("Введите число");
+int value = int.Parse(Console.ReadLine());
+int sum = value*value;
+Console.WriteLine("Квадрат числа равен:"+sum);
